c22a55865a6d71dcebd51137c0c3c0a53a4e49dc
nam9nine <southgyuios19@gmail.com>
2024-12-02 10:11:08 +0900

    서버, 클라이언트 메세지 송수신 테스트

--- client/root.c
+++ client/root.c
@@ -3,6 +3,9 @@
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+
 int main()
 {
     printf("hello client\n");
@@ -17,20 +20,34 @@ int main()
     }
 
     server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(8080);
-    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(8081);
+    server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     if (connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
     {
         perror("connect 실패");
+        close(client_socket);
         return 1;
     }
 
-    char server_response[100];
-    recv(client_socket, &server_response, sizeof(server_response), 0);
+    while (1)
+    {
+        char client_message[100];
+        memset(client_message, '\0', sizeof(client_message));
+        printf("메세지를 입력하시오 : ");
 
-    printf("서버 응답: %s\n", server_response);
+        fgets(client_message, sizeof(client_message), stdin);
+        client_message[strcspn(client_message, "\n")] = '\0'; // 입력받은 문자열 끝에 있는 개행 문자 제거
 
-    close(client_socket);
+        if (strcmp(client_message, "END") == 0)
+        {
+            break;
+        }
+        send(client_socket, client_message, strlen(client_message), 0);
+    }
+    if (close(client_socket) < 0)
+    {
+        perror("client_socket 닫기 실패");
+    }
     return 0;
 }
