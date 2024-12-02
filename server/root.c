b03b7a88bc208b75475dea877d25417eb30256e5
nam9nine <southgyuios19@gmail.com>
2024-12-02 10:05:48 +0900

    서버, 클라이언트 메세지 송수신 테스트

--- server/root.c
+++ server/root.c
@@ -4,13 +4,16 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+#include <signal.h>
+#include <stdlib.h>
 
-void ListenServer();
-int main()
+int server_socket;
+
+void handle_signal(int sig)
 {
-    printf("Hello, Server!\n");
-    ListenServer();
-    return 0;
+    printf("\n서버 종료 중...\n");
+    close(server_socket);
+    exit(0);
 }
 
 void ListenServer()
@@ -21,11 +24,11 @@ void ListenServer()
 
     // 서버 소켓 설정
     socketInfo.sin_family = AF_INET;
-    socketInfo.sin_port = htons(8080);
+    socketInfo.sin_port = htons(8081);
     socketInfo.sin_addr.s_addr = INADDR_ANY;
 
     socklen_t sockLen = sizeof(socketInfo);
-    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket < 0)
     {
         perror("socket 생성 실패");
@@ -51,21 +54,62 @@ void ListenServer()
     printf("서버가 클라이언트 접속을 기다립니다...\n");
 
     socklen_t clientLen = sizeof(clientInfo);
-    int client_socket = accept(server_socket, (struct sockaddr *)&clientInfo, &clientLen);
-    if (client_socket < 0)
+
+    while (1)
     {
-        perror("accept 실패");
-        close(server_socket);
-        return;
+        int client_socket = accept(server_socket, (struct sockaddr *)&clientInfo, &clientLen);
+        if (client_socket < 0)
+        {
+            perror("accept 실패");
+            close(server_socket);
+            return;
+        }
+
+        printf("클라이언트 연결 성공: %s\n", inet_ntoa(clientInfo.sin_addr));
+        printf("클라이언트 주소 : %s\n", inet_ntoa(clientInfo.sin_addr));
+        // 클라이언트에게 메시지 전송
+
+        while (1)
+        {
+            char request_message[100];
+            memset(request_message, '\0', sizeof(request_message));
+            int bytes_received = recv(client_socket, request_message, sizeof(request_message), 0);
+            if (bytes_received <= 0)
+            {
+                if (bytes_received == 0)
+                {
+                    // 클라이언트가 정상적으로 연결을 종료
+                    printf("클라이언트가 연결을 종료했습니다.\n");
+                    if (close(client_socket) < 0)
+                    {
+                        perror("클라이언트 소켓 닫기 실패");
+                    }
+                    break;
+                }
+                else if (bytes_received < 0)
+                {
+                    // 오류 처리
+                    perror("recv 오류");
+                    if (close(client_socket) < 0)
+                    {
+                        perror("클라이언트 소켓 닫기 실패");
+                    }
+                    break;
+                }
+            }
+            printf("클라이언트가 보낸 메세지 : %s\n", request_message);
+        }
     }
+    if (close(server_socket) < 0)
+    {
+        perror("서버 소켓 닫기 실패");
+    }
+}
 
-    printf("클라이언트 연결 성공: %s\n", inet_ntoa(clientInfo.sin_addr));
-
-    // 클라이언트에게 메시지 전송
-    char server_message[100] = "Hello, I am the server.";
-    send(client_socket, server_message, strlen(server_message), 0);
-
-    // 소켓 닫기
-    close(client_socket);
-    close(server_socket);
+int main()
+{
+    signal(SIGINT, handle_signal);
+    printf("Hello, Server!\n");
+    ListenServer();
+    return 0;
 }
